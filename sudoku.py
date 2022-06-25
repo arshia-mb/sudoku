aed8ce6eedf7ef0026ee125dbbf843342ea59029
alast0r <alastorofploys@gmail.com>
2022-06-25 07:53:05 +0430

    Added forwardchecking and getting each constraint

--- sudoku.py
+++ sudoku.py
@@ -4,7 +4,27 @@
 from csp import CSP
 from csp import Variabel
 
+#Finds the avialbe values for a variable - Domain
+def getDomain(csp:CSP,var:Variabel) -> list:
+    all_domain = csp.domain
+    variabels = csp.vars
+    if var.value != 0:
+        return [var.value]
+    domain = all_domain.copy() #All the values that can be assigned
+    #Check which ones are viabel
+    for index in var.friends:
+        if variabels[index].value in domain:
+            domain.remove(variabels[index].value)
+    return domain
 
+#Checks the assignment of the values is okay or not
+def forwardcheck(assignment:list,domain:list,size:int) -> bool:
+    csp = CSP(assignment,domain,size)
+    for i in range(size*size):
+        domain = getDomain(csp,csp.vars[i])
+        if len(domain) == 0:
+            return False
+    return True
 
 if __name__ == "__main__":
     domain = [1,2,3]
@@ -16,6 +36,16 @@ if __name__ == "__main__":
 
     print(csp.consistent())
     print(csp.complete())
+
+    vars = csp.vars
+    for i in range(size*size):
+        print(getDomain(csp,csp.vars[i]),end=" ")
+        if i%size == size-1:
+            print("") 
+
+
+
+
 
 
     
